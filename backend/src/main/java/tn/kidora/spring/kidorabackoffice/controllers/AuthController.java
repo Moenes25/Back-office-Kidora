396059a1474aee193fbf0fcdbb45efd44d35330e
beissmoh <beissmoh@outlying.works>
2025-11-19 13:07:48 +0100

    jwt configuration

---
+++ backend/src/main/java/tn/kidora/spring/kidorabackoffice/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package tn.kidora.spring.kidorabackoffice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/auth")
+@AllArgsConstructor
+public class AuthController {
+ 
+
+    private 
+    
+}
