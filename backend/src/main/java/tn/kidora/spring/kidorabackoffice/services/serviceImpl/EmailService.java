b68458d9e21cb13f53e4d9141950aae7b9d2d96f
arwadridi <arwadridi624@gmail.com>
2025-12-04 12:05:42 +0100

    Reset password + configuration email

--- backend/src/main/java/tn/kidora/spring/kidorabackoffice/services/serviceImpl/EmailService.java
+++ backend/src/main/java/tn/kidora/spring/kidorabackoffice/services/serviceImpl/EmailService.java
@@ -11,7 +11,7 @@ public class EmailService {
     private final JavaMailSender mailSender;
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("ton.email.test@gmail.com"); // même que username
+        message.setFrom("arwadridi624@gmail.com"); // même email
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
