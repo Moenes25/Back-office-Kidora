f636afabcc1c6525f35b15d3364f86b9e47c20b4
sidi-med3 <sidimedmedmoctar691@gmail.com>
2026-01-04 15:18:31 +0100

    configure cors

--- backend/src/main/java/tn/kidora/spring/kidorabackoffice/config/SecurityConfig.java
+++ backend/src/main/java/tn/kidora/spring/kidorabackoffice/config/SecurityConfig.java
@@ -65,6 +65,19 @@ public class SecurityConfig {
              .build();
                
     }
+
+     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     }
 
     
